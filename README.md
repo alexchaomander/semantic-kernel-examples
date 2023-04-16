d06b92352e7f21ee0dd46414b1e8b3613ecec5ac
Alex Chao <achao@achao.localdomain>
2023-04-16 08:29:39 -0700

    Added paul-graham-essay-qa example

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # semantic-kernel-examples
 Examples using the Semantic Kernel in Python
+
+# Get Started with Semantic Kernel ⚡
+
+Install the latest package:
+
+    python -m pip install --upgrade semantic-kernel
+
+
+# AI backends
+
+## OpenAI / Azure OpenAI API keys
+
+Make sure you have an
+[Open AI API Key](https://openai.com/api/) or
+[Azure Open AI service key](https://learn.microsoft.com/azure/cognitive-services/openai/quickstart?pivots=rest-api)
+
+Copy those keys into a `.env` file (see the `.env.example` file):
+
+```
+OPENAI_API_KEY=""
+OPENAI_ORG_ID=""
+AZURE_OPENAI_DEPLOYMENT_NAME=""
+AZURE_OPENAI_ENDPOINT=""
+AZURE_OPENAI_API_KEY=""
+```
---
+++ paul-graham-essay-qa/paul_graham_chat.py
@@ -0,0 +1,102 @@
+import asyncio
+import pickle
+
+import semantic_kernel as sk
+from semantic_kernel.ai.open_ai import OpenAITextCompletion, OpenAITextEmbedding, OpenAIChatCompletion
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import streamlit as st
+
+print("Loading text data...")
+with open("../data/paul_graham_essay.txt", "r") as f:
+    document = f.read()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 20,
+    length_function = len,
+)
+
+print("Splitting the text data into chunks...")
+texts = text_splitter.create_documents([document])
+len(texts)
+
+def build_kernel() -> sk.Kernel:
+    # Setup kernel with OpenAI completion and embedding backends
+    api_key, org_id = sk.openai_settings_from_dot_env()
+
+    kernel = (
+        sk.kernel_builder()
+        .configure(lambda c: c.add_text_backend("completion", OpenAITextCompletion("text-davinci-003", api_key, org_id)))
+        .configure(lambda c: c.add_embedding_backend("embeddings", OpenAITextEmbedding("text-embedding-ada-002", api_key, org_id)))
+        .configure(lambda c: c.add_chat_backend("chat-gpt", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id)))
+        .with_memory_storage(sk.memory.VolatileMemoryStore())
+        .build()
+    )
+
+    kernel.import_skill(sk.core_skills.TextMemorySkill())
+
+    return kernel
+
+sk_prompt = """
+ChatBot can have a conversation with you about any topic.
+It can give explicit instructions or say 'I don't know' if
+it does not have an answer.
+
+Use this primary source information if relevant:
+{{recall $user_input}}
+
+Chat:
+{{$chat_history}}
+User: {{$user_input}}
+ChatBot: """.strip()
+
+print("Building the kernel...")
+kernel = build_kernel()
+
+chat_func = kernel.create_semantic_function(sk_prompt, max_tokens=200, temperature=0.8)
+
+context = kernel.create_new_context()
+
+context[sk.core_skills.TextMemorySkill.COLLECTION_PARAM] = "primarySource"
+context[sk.core_skills.TextMemorySkill.RELEVANCE_PARAM] = 0.8
+
+context["chat_history"] = ""
+
+print("Creating memories...")
+with open("kernel_storage_store.pkl", "rb") as f:
+    kernel.memory._storage._store = pickle.load(f)
+
+# UNCOMMENT THIS SECTION IF YOU WANT TO BUILD YOUR OWN EMBEDDINGS
+# collection_name = "paul-graham-essays"
+# print("Adding texts into the Semantic Kernel's memory")
+# for idx, value in enumerate(texts):
+#     asyncio.run(kernel.memory.save_reference_async(
+#         collection=collection_name,
+#         description=value.page_content,
+#         text=value.page_content,
+#         external_id=idx,
+#         external_source_name=collection_name
+#     ))
+#     print("URL {} saved".format(idx))
+
+print("Initializing Streamlit chat")
+st.title("Chat with PaulG")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    """
+    This is a web application that allows you to interact with 
+    the Paul Graham, founder of YCombinator.
+    Enter a **query** in the **text box** and **press enter** to receive 
+    a **response**
+    """
+    )
+
+# Get user input
+user_query = st.text_input("Enter question here, to exit enter :q", "What are Paul's favorite startups to invest in?")
+if user_query != ":q" or user_query != "":
+    # Pass the query to the ChatGPT function
+    context['user_input'] = user_query
+    response = asyncio.run((kernel.run_on_vars_async(context.variables, chat_func)))
+    st.write(f"{user_query}\n\n {response}")
